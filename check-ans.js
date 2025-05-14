a274ea3a738b4d4754723c77a50e68ca17c3c73b
Jason Cheng <jasonic.c.7134@gmail.com>
2025-05-14 10:42:45 -0400

    feat: extra `posttalk` info

--- check-ans.js
+++ check-ans.js
@@ -102,7 +102,13 @@ document.getElementById('submit').addEventListener('click', async () => {
     ${language != ans.value ? "<span style='color: red; font-size: 1.2rem'>You lose...</span>" : "<span style='color: green'>You win!</span>"}<br><br>
 
     This snippet was <strong><code class="code-inline special-code" style="text-decoration: none;">${language}</code></strong> code from the repository <a href="${"https://github.com/" + repo.full_name}" target="_blank"><code class="code-inline special-code">${repo.full_name}</code></a> from lines ${dupletLines[1]} of file <a href="${url}" target="_blank">
-    <code class="code-inline special-code">${selectedFile.path.split("/")[1] ? selectedFile.path.split("/")[1] : selectedFile.path}</code></a>.</div>`
+    <code class="code-inline special-code">${selectedFile.path.split("/")[1] ? selectedFile.path.split("/")[1] : selectedFile.path}</code></a>. ${language != ans.value ? "Better luck next time..." : "Keep it up!"}
+    
+    <br><br>
+    Press the 'Run Snippet' button to try again.
+    </div>
+    
+`
 
 });
 });
